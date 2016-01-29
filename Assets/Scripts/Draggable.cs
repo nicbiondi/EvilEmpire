bffa899c4a1fa246c4d0578471f0f0fb6e6ea1c7
nicbiondi <nicbiondi@gmail.com>
2016-01-28 20:36:40 -0800

    Implemented broad drag and drop functionality that allows dragging tiles and dropping into specified areas.

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IDragHandler,IBeginDragHandler,IEndDragHandler{
+	public Transform originalParent = null;
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		originalParent = transform.parent;
+		transform.SetParent(transform.parent.parent);
+		GetComponent<CanvasGroup>().blocksRaycasts = false;
+	}
+	public void OnDrag(PointerEventData eventData)
+	{
+		transform.position = eventData.position;
+	}
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		transform.SetParent(originalParent);
+		GetComponent<CanvasGroup>().blocksRaycasts = true;
+	}
+}
---
+++ Assets/Scripts/DropZone.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler{
+
+	public void OnPointerEnter(PointerEventData eventdata)
+	{
+	}
+	public void OnPointerExit(PointerEventData eventdata)
+	{
+	}
+	public void OnDrop(PointerEventData eventdata)
+	{
+	 Draggable drag = eventdata.pointerDrag.GetComponent<Draggable>();
+	 if(drag != null)
+	 {
+	 	drag.originalParent = transform;
+	 }
+	}
+}
